a109cb976e9cb58f2ee678a400567331b26584ed
Aditya Gawade <adityagawade.rmdstic.it@gmail.com>
2026-08-02 15:10:08 +0530

    marge2 array

--- Marge_2_sorted_array.py
+++ Marge_2_sorted_array.py
@@ -15,4 +15,25 @@ def marge_array(num1, num2):
             i += 1
 
         if (num2[j]<num1[i]):
-            if (len(result)==0 or result[-1] != num1)
+            if (len(result)==0 or result[-1] != num1):
+                result.append(num2[j])
+            j += 1
+
+    while(i < n):
+        if(len(result)==0 or result[-1] != num1[i]):
+            result.append(num1[i])
+        i += 1
+
+    while(j < m):
+        if (len(result)==0 or result[-1] != num1):
+            result.append(num2[j])
+        j += 1
+
+    return(result)
+        
+def main():
+    num1 = list(map(int, input().split()))
+    num2 = list(map(int, input().split()))
+    a = marge_array(num1 , num2)
+    print(a)
+main()
