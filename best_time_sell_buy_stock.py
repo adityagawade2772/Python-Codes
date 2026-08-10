25802d1699c5b517cef634f4cb31ca084e016c12
Aditya Gawade <adityagawade.rmdstic.it@gmail.com>
2026-08-10 23:21:28 +0530

    stock

--- best_time_sell_buy_stock.py
+++ best_time_sell_buy_stock.py
@@ -8,10 +8,20 @@ def brout(arr): #O(n^2)
 
     return max_profit
 
+def optimal(prices):
+    max_profit = 0
+    min_price = float("inf")
+    n = len(prices)
+    for i in range(0,n):
+        min_price = min(prices[i], min_price)
+        max_profit = max(max_profit, prices[i]- min_price)
+
+    return max_profit
+
 
 def main():
     n = list(map(int, input().split()))
-    a = brout(n)
+    a = optimal(n)
     print(a)
     
 main()
